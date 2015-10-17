0672616f99ec902c03895ad69106d805cbc813e6
ncbci <chenstine@yahoo.com.tw>
2015-10-17 22:48:54 +0800

    Implement Q4 (recursive and iterative version)
    execution time:
    	-recursive: 0.003005 sec
    	-iterative: 0.006161 sec

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,55 @@
-/* FIXME: Implement! */
+/* iterative version */
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+int maxSubArray(int A[], int n)
+{
+	int head, tail, i, temp, sum = A[0];
+	for (head = 0; head < n; ++head) {
+		for (tail = head; tail < n; ++tail) {
+			temp = 0;
+			for (i = head; i < tail; ++i) {
+				temp += A[i];
+			}
+			if (temp > sum) sum = temp;
+		}
+	}
+
+	return sum;
+}
 
 int main()
 {
+    struct timespec start, end;
+	int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int i, sum;
+
+    /* compute the execution time */
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    for (i = 0; i < 10000; ++i) {
+		sum = maxSubArray(A, sizeof(A)/sizeof(A[0]));
+    }
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    double cpu_time = diff_in_second(start, end);
+
+	printf("Sum: %d\n", sum);
+    printf("execution time: %lf sec\n", cpu_time);
+
     return 0;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,56 @@
-/* FIXME: Implement! */
+/* recursive version */
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+int maxSubArray(int A[], int n)
+{
+	int sum = A[0], i, temp = 0, sum2 = A[0];
+
+	if (n > 1) {
+		sum2 = maxSubArray(A+1, n-1);
+	}
+
+	for (i = 0; i < n; ++i) {
+		temp += A[i];
+		if (temp > sum) sum = temp;
+	}
+
+	if (sum > sum2) return sum;
+	else return sum2;
+}
 
 int main()
 {
+    struct timespec start, end;
+	int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int i, sum;
+
+    /* compute the execution time */
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    for (i = 0; i < 10000; ++i) {
+		sum = maxSubArray(A, sizeof(A)/sizeof(A[0]));
+    }
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    double cpu_time = diff_in_second(start, end);
+
+	printf("Sum: %d\n", sum);
+    printf("execution time: %lf sec\n", cpu_time);
+
     return 0;
 }
